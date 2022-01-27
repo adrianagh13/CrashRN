51947546c9e58b65d595d906bcd63bd008655c6c
adrianagh13 <gomezhinojosa.adriana@gmail.com>
2022-01-26 23:08:17 -0600

    Initial commit Some structures already created, finished custom components lesson

---
+++ App.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import {FlatList, StyleSheet, View} from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
+export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  /*const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      {id: Math.random().toString(), value: enteredGoal},
+    ]);
+  };
+  we cant use the function like this because it has enteredGoal which is the state stored in goalinput
+  so we are going to name it differently and make dure that we forward that variable to GoalInput
+  */
+
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [
+      //shorter statement of return
+      ...currentGoals,
+      {id: Math.random().toString(), value: goalTitle},
+    ]);
+  };
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter(goal => goal.id !== goalId);
+    });
+  };
+
+  //RETURNS use parentheses when they are going to render some item
+  return (
+    <View style={styles.screen}>
+      <GoalInput onAddGoal={addGoalHandler} />
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => (
+          <GoalItem
+            id={itemData.item.id}
+            onDelete={removeGoalHandler}
+            title={itemData.item.value}
+          />
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+});
---
+++ components/GoalInput.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Button, StyleSheet, TextInput, View} from 'react-native';
+import {useState} from 'react/cjs/react.development';
+
+const GoalInput = props => {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+
+  //to set up an argument that should be past along we can use an anonymous func or bind
+  //bind indicates arguments that should be received when the function gets called, in this case is the onPress
+  //this way now we can add enteredGoal to the function
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        placeholder="Course Goal"
+        style={styles.input}
+        onChangeText={goalInputHandler}
+        value={enteredGoal}
+      />
+      <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+export default GoalInput;
